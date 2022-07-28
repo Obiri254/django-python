84933044ed840e6639b30f79e3cea483c96f5ed1
Obiri Anthony <49581603+Obiri254@users.noreply.github.com>
2022-07-28 16:15:12 +0300

    Add files via upload

---
+++ django_project/blog/admin.py
@@ -0,0 +1,5 @@
+from atexit import register
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
---
+++ django_project/blog/views.py
@@ -0,0 +1,16 @@
+from multiprocessing import context
+from django.shortcuts import render
+from .models import Post
+
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
+
+
